97681f9fb54240bd7a3cf9c30743ff905280523f
natachadpd <natacha.duarte@zup.com.br>
2022-05-09 14:29:51 -0300

    Adicionando pasta unitarios

--- consumer/src/test/java/pact/TaskConsumerContractTest.java
+++ consumer/src/test/java/pact/TaskConsumerContractTest.java
@@ -8,51 +8,43 @@ import au.com.dius.pact.core.model.annotations.Pact;
 import br.ce.wcaquino.consumer.tasks.model.Task;
 import br.ce.wcaquino.consumer.tasks.service.TasksConsumer;
 import org.apache.http.client.ClientProtocolException;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class TaskConsumerContractTest {
 
-    //rules do JUnit
-    //isso vai criar a instância mockapi. Abaixo estamos tentando acessar a api tasks. Abaixo lado do servidor e embutido no codigotem a
-    //inteligência para criar a api mockada
     @Rule
     public PactProviderRule mockProvider = new PactProviderRule("Tasks",this);
-    // PactProviderRule mockProvider = new PactProviderRule("Tasks","localhost", 8080 , this); outra forma
 
-    //expectativa que a api mockada deve ter, vou dizer como pretendo interagir com ela e o que deve fazer quando fizer a interação
-    //no final será gerado um contrato entre basic consumer e tasks (linha 13)
     @Pact(consumer = "BasicConsumer")
-    // metodo que vai configurar essa espectativa
+
     public RequestResponsePact createPact(PactDslWithProvider builder) {
         return builder
-                .given("There is a task with id = 1") //onde informo minhas pre condições, o que preciso ter para que o teste seja executado, funcione
-                .uponReceiving("Retrieve Task #1") //quando acontecer determinada ação, faça isso (quando vier um get na task de id 1
-                        .path("/todo/1") //o caminho que ele vai buscar na api será este
-                        .method("GET") //utilizando GET
-                .willRespondWith() //quero que me responda desta forma
-                        .status(200) //vai responder um status code 200
-                        .body("{\"id\": 1, \"task\": \"Task from pact\", \"dueDate\": \"2020-01-01\"}") //tambem vai trazer o copro do json
-                .toPact(); //builder finaiza convertendo tudo isso para um pacto
+                .given("There is a task with id = 1")
+                .uponReceiving("Retrieve Task #1")
+                        .path("/todo/1")
+                        .method("GET")
+                .willRespondWith()
+                        .status(200)
+                        .body("{\"id\": 1, \"task\": \"Task from pact\", \"dueDate\": \"2020-01-01\"}")
+                .toPact();
     }
 
     @Test
-    @PactVerification //com isso sabe que precisa fazer verificação neste ponto
+    @PactVerification
     public void test() throws ClientProtocolException, IOException {
-        //Arrange
+
         TasksConsumer consumer = new TasksConsumer(mockProvider.getUrl());
         System.out.println(mockProvider.getUrl());
-        //Act
-        Task task = consumer.getTask(1L);//passamos o id 1 como esperado no pacto
+
+        Task task = consumer.getTask(1L);
         System.out.println(task);
-        //Assert
+
         assertThat(task.getId(), is(1L));
         assertThat(task.getTask(), is("Task from pact"));
     }
--- consumer/src/test/java/unitarios/TasksConsumerTest.java
+++ consumer/src/test/java/unitarios/TasksConsumerTest.java
@@ -1,3 +1,5 @@
+package unitarios;
+
 import br.ce.wcaquino.consumer.tasks.model.Task;
 import br.ce.wcaquino.consumer.tasks.service.TasksConsumer;
 import br.ce.wcaquino.consumer.utils.RequestHelper;
@@ -15,8 +17,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.BDDMockito.then;
-
 @RunWith(MockitoJUnitRunner.class)
 public class TasksConsumerTest {
 
@@ -25,31 +25,25 @@ public class TasksConsumerTest {
     @InjectMocks
     private TasksConsumer consumer = new TasksConsumer(INVALID_URL);
 
-    //mockando
     @Mock
     private RequestHelper helper;
 
     @Test
     public  void shoulGetAndExistingTask() throws ClientProtocolException, IOException {
 
-        //Arrange -> configurar o que é necessário para o teste
-        //ibjeto que vai retornar
+
         Map<String, String> expectedTask = new HashMap<String, String>();
-        //colocar no map todos os valores que quero que retorne, nisso farei um put para inserir no map um conjunto de chave , valor
+
         expectedTask.put("id","1");
         expectedTask.put("task","TaskMocked!");
         expectedTask.put("dueDate","2000-01-01");
 
-        //when > quando uma determinada ação ocorrer então deve fazer outra coisa. Quando o metodo helper. ocorrer com determinado parametro,
-        //o parametro é o que definimos na linha 23 + as concatenações. "then" então deve no nosso caso retornal null
         Mockito
                 .when(helper.get(INVALID_URL + "/todo/1"))
-                .thenReturn(expectedTask); //retorna objeto moclado linha 36,38.39.40
+                .thenReturn(expectedTask);
 
-        //Act -> Execução da tarefa que queremos testar
         Task task = consumer.getTask(1L);
 
-        // Assert -> Realmente ocorreu como era esperado?
         Assert.assertNotNull(task);
         Assert.assertThat(task.getId(), CoreMatchers.is(1L));
 
